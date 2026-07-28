45db0e2eaf27d1e79064c20240ce8c5acaee2279
Driv-en <jblood@digitalsafetyinspection.com>
2026-07-28 11:06:09 -0500

    Add user database operations in users.js

---
+++ public/db/users.js
@@ -0,0 +1,8 @@
+module.exports = {
+    async findAll() {},
+    async findById(id) {},
+    async findByEmail(email) {},
+    async create(data) {},
+    async update(id, data) {},
+    async delete(id) {}
+};
