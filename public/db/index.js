1c727a70f31d6c4d57420070c949e60a662cd9d6
Driv-en <jblood@digitalsafetyinspection.com>
2026-07-28 11:05:13 -0500

    Create index.js to export database modules

---
+++ public/db/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    users: require('./users'),
+    projects: require('./projects'),
+    equipment: require('./equipment'),
+    employees: require('./employees'),
+    auditLogs: require('./audit-logs'),
+    pm: require('./pm'),
+    inspections: require('./inspections'),
+    workOrders: require('./work-orders')
+};
