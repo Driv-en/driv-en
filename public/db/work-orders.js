6d04a9a5857c99f984d64235241b6d4cea4855be
Driv-en <jblood@digitalsafetyinspection.com>
2026-07-28 11:17:40 -0500

    Add work-orders.js with CRUD operations

---
+++ public/db/work-orders.js
@@ -0,0 +1,8 @@
+module.exports = {
+    async findAll() {},
+    async findById(id) {},
+    async findAllByCustomer(customerId) {},
+    async create(data) {},
+    async update(id, data) {},
+    async delete(id) {}
+};
