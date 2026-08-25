f36a434ecdbe1d562621440c841977e21e9d35e6
Driv-en <jblood@digitalsafetyinspection.com>
2026-08-25 14:20:10 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,10 +4,30 @@ const ASSETS = [
   '/index.html',
   '/onboarding-key-personnel.html',
   '/onboarding-dashboard.html',
+  '/onboarding-clients.html',
+  '/onboarding-divisions.html',
+  '/onboarding-employees.html',
+  '/onboarding-projects.html',
+  '/login.html',
+  '/admin.html',
+  '/equipment.html',
+  '/equipment-detail.html',
+  '/inspections.html',
+  '/inspection-detail.html',
+  '/pm.html',
+  '/pm-detail.html',
+  '/workorders.html',
+  '/workorder-detail.html',
+  '/module-selection.html',
+  '/change-password.html',
+  '/cart.html',
+  '/checkout.html',
   '/logo.png',
   '/icons/favicon.png',
   '/icons/driven-icon-192.png',
-  '/icons/driven-icon-512.png'
+  '/icons/driven-icon-512.png',
+  '/css/theme.css',
+  '/js/theme.js'
 ];
 
 self.addEventListener('install', e => {
